ec0e3011ce0f9f8e842aa17f7da4341e07a9ae89
Damien Lecan <dev@dlecan.com>
2017-08-04 17:18:20 +0200

    Useless

--- _site/js/generate_summary.js
+++
@@ -1,5 +0,0 @@
-$('h2').each(function(id){
-    var id=this.id;
-    var nom = this.textContent;
-	$('#summary').append('<li><a href="#'+id+'" style="text-decoration: none;">'+nom+'</a></li>');
-});
